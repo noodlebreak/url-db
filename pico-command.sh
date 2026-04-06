13673079cc62b65d1860fac4217dcf8e347a0701
Ashish Kumar <ashish.kumar.contractor@upkeep.com>
2026-04-06 16:33:30 +0530

    Ignore GA4-EVENTS.md locally and exclude from rsync

--- pico-command.sh
+++ pico-command.sh
@@ -1,3 +1,3 @@
 # Note: gokr-rsync errors if --delete tries to remove a pre-existing remote git-hooks/ dir
 # (excluded locally). Omit --delete; overwrite files by name. Prune orphans manually if needed.
-rsync -rv --exclude '.git*' --exclude 'pico-command.sh' --exclude 'git-hooks/' --exclude 'bookmark*' ../url-db/ pgs.sh:/url-db
+rsync -rv --exclude '.git*' --exclude 'pico-command.sh' --exclude 'git-hooks/' --exclude 'GA4-EVENTS.md' --exclude 'bookmark*' ../url-db/ pgs.sh:/url-db
